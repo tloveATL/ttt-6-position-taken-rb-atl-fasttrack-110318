5a372844fc1a10cf033e56491240defffe827017
Tiffani Weaver <tlove.weaver@gmail.com>
2019-02-17 17:52:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,8 @@
 def position_taken?(board, index)
-  if board[0,1,2,3,4,5,6,7,8] == " " || ""
-  return false
-end
-
-def position_taken?(board, index)
-   if board[0,1,2,3,4,5,6,7,8] != " " || ""
-  return true
+  position = board[index]
+  if position == " " || position == "" || position == nil
+    return false
+else
+    return TRUE
+  end
 end
